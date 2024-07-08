d777f22c7efee39758989b77846e3fde4c32b94b
archanaa0612 <115165862+archanaa0612@users.noreply.github.com>
2024-07-08 13:56:51 +0530

    Create Number_of_Provinces.cpp

---
+++ 15.Graph/Problems on DFS or BFS/Number_of_Provinces.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    
+    vector<vector<int>> convertToList(vector<vector<int>> adj,int V)
+    {
+        vector<vector<int>> adjList(V);
+        for(int i =0;i<V;i++)
+        {
+            for(int j =0;j<V;j++)
+            {
+                if(i!=j && adj[i][j])
+                {
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
+            }
+        }
+        return adjList;
+    }
+    
+    void dfs(int node , vector<int> &visited,vector<vector<int>> adjList)
+    {
+        visited[node]=1;
+        for(auto it : adjList[node])
+        {
+            if(!visited[it]) dfs(it,visited,adjList);
+        }
+    }
+  int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<vector<int>> adjList =convertToList(isConnected,V);
+        int ans =0;
+        vector<int> visited(V,0);
+        for(int i =0;i<V;i++)
+        {
+            if(!visited[i])
+            {
+                ans++;
+                dfs(i,visited,adjList);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.findCircleNum(adj) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
