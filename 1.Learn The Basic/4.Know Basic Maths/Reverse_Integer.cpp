7933f3ea4265e6b8b7dfaef4f72c2ce1c6acdbae
archanaa0612 <115165862+archanaa0612@users.noreply.github.com>
2024-08-07 11:59:40 +0530

    Create Reverse_Integer.cpp

---
+++ 1.Learn The Basic/4.Know Basic Maths/Reverse_Integer.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        long r=0;    
+        while(x){
+         r=r*10+x%10;
+         x=x/10; 
+        }
+        if(r>INT_MAX || r<INT_MIN) return 0; 
+        return int(r);
+    }
+};
+
+int main()
+{
+  int x;
+  cin>>x;
+  Solution obj;
+  int ans = obj.reverse(x);
+  cout<<ans;
+  return 0;
+}
