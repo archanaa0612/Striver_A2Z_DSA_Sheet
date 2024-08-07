a43814d2ce21c09a0d27d3715fd37df15fd30958
archanaa0612 <115165862+archanaa0612@users.noreply.github.com>
2024-08-07 12:53:18 +0530

    Create Print_N_to_1_without_loop.cpp

---
+++ 1.Learn The Basic/5.Learn Basic Recursion/Print_N_to_1_without_loop.cpp
@@ -0,0 +1,38 @@
+/*
+Time Complexity: O(N).
+Auxiliary Space: O(N) (Recursive)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void printNos(int N) {
+        if(N==1){
+            cout<<N<<" ";
+            return;
+        }
+        cout<<N<<" ";
+        printNos(N-1);
+    }
+};
+
+int main() {
+    int T;
+
+    // taking testcases
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        // input N
+        cin >> N;
+        Solution ob;
+        // calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
